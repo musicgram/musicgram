9978e96b8c4149d757c44f0ad84c28cd925710b3
ivan <ivansu.webmail@gmail.com>
2018-09-13 19:48:18 +0700

    findAll musics

--- controllers/music.js
+++ controllers/music.js
@@ -1,10 +1,25 @@
+const Music = require('../models/musicModel');
+
 module.exports = {
-    upload : function(req,res){
-        
-            res.send({
-              status: 200,
-              message: 'Your file is successfully uploaded',
-              link: req.file.cloudStoragePublicUrl
-            })    
-    }
+  findAll: (req, res) => {
+    Music.find()
+    .then(musics => {
+      res.status(200).json({
+        message: 'success get all musics',
+        musics: musics
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: err.message
+      });
+    })
+  },
+
+  upload: (req,res) => {
+    res.status(200).json({
+      message: 'Your file is successfully uploaded',
+      link: req.file.cloudStoragePublicUrl
+    });
+  }
 }
