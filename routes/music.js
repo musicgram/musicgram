cf340c069b446914a5c013a433e309f1d912f8df
ivan <ivansu.webmail@gmail.com>
2018-09-13 20:05:36 +0700

    add route for findAll Musics

--- routes/music.js
+++ routes/music.js
@@ -2,9 +2,12 @@ const routes = require('express').Router()
 const controller = require('../controllers/index')
 const upload = require('../helpers/index')
 
-routes.post('/upload',
-  upload.multer.single('image'), 
+routes.get('/musics', controller.music.findAll);
+routes.post(
+  '/upload',
+  upload.multer.single('music'), 
   upload.sendUploadToGCS,
-  controller.music.upload)
+  controller.music.upload
+);
 
-  module.exports = routes
+module.exports = routes;
