78116470f6bda409a4a87d5aec58f61794355ae2
julianjca <julianjca11@gmail.com>
2018-09-14 08:42:16 +0700

    upload

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const { register, login, getUsers,addMusic } = require('../controllers/user_controller')
+
 const auth = require('../middlewares/index')
 
 router.post('/register',register)
@@ -7,5 +8,6 @@ router.post('/register',register)
 router.post('/login',login)
 
 router.get('/',getUsers)
+
 router.post('/addMusic',auth,addMusic)
 module.exports = router
