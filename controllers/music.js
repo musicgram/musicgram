6ca22396d03d1072f5001aa97bf7ad4facfbfddd
Julian Christian Anderson <julianjca11@gmail.com>
2018-09-14 08:14:08 +0700

    Revert "Upload"

--- controllers/music.js
+++ controllers/music.js
@@ -1,26 +1,10 @@
-const Music = require('../models/musicModel.js')
-
 module.exports = {
-    upload : function(req,res){        
+    upload : function(req,res){
+        
             res.send({
               status: 200,
               message: 'Your file is successfully uploaded',
               link: req.file.cloudStoragePublicUrl
             })    
-    },
-    create : function(req,res){
-        Music.create({
-            title : req.body.title,
-            file : req.body.file,
-            user : req.user.id
-        })
-        .then(newMusic =>{
-            res.status(200).json({
-                musicId : newMusic._id
-            })
-        })
-        .catch(err =>{
-            res.status(400).json(err)
-        })
     }
 }
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,11 @@
 const router = require('express').Router()
-
-const auth = require('../middlewares/index')
-const { register, login, getUsers, verifyUser,addMusic } = require('../controllers/user_controller')
+const { register, login, getUsers, verifyUser } = require('../controllers/user_controller')
 
 router.post('/register',register)
 
 router.post('/login',login)
 
 router.get('/',getUsers)
-router.post('/addMusic',auth,addMusic)
 
 router.get('/verify',verifyUser)
 
